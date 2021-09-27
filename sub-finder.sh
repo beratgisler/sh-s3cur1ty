98a11282cd70f9a5c1e20192c7748ffdf079883a
Berat Gökberk İşler <beratisler_@hotmail.com>
2021-09-27 18:07:28 +0300

    Update sub-finder.sh
    clear

--- sub-finder.sh
+++ sub-finder.sh
@@ -3,13 +3,13 @@ echo '*********************************'
 echo '************SUBFINDER************'
 echo '*********************************'
 echo ' waiting for processing .. .. .. .. ' 
-wget -q omu.edu.tr
+wget -q xxx.edu.tr
 
-cat index.html | grep "href=" | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | sort -u > omu-sub.txt
+cat index.html | grep "href=" | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | sort -u > xxx-sub.txt
 
-cat omu-sub.txt | cut -d "'" -f 1 | grep .edu.tr > sublist.txt
+cat xxx-sub.txt | cut -d "'" -f 1 | grep .edu.tr > sublist.txt
 
-rm -r omu-sub.txt
+rm -r xxx-sub.txt
 rm -r index*
 
 echo '//// SUBDOMAINS \\\\'
